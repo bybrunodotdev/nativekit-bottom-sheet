e229f726fc86cbba8f8145f1fc53b59788b46da8
Bruno Andrade <hey@brunoandrade.me>
2020-01-15 06:11:01 -0300

    add: project example with initial component

---
+++ example/AwesomeBottomSheet/Components/BottomSheet.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface BottomSheetProps {}
+
+function BottomSheet(): BottomSheetProps {
+  return (
+    <View>
+      <Text>My BottomSheet</Text>
+    </View>
+  );
+}
+
+export default BottomSheet;
